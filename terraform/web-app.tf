5e28079f53c8e50f356b0e2a611afbdf8706e4de
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-04 08:17:05 +0100

    Adding identity storage account and outputting connection string

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -3,6 +3,18 @@ resource "azurerm_resource_group" "resource-group" {
     location = var.region
 }
 
+resource "azurerm_storage_account" "identity-storage" {
+    name = "identitysa${var.environment}"
+    resource_group_name = azurerm_resource_group.resource-group.name
+    location = azurerm_resource_group.resource-group.location
+    account_tier = "Standard"
+    account_replication_type = "LRS"
+}
+
+output "identity_storage_connection" {
+  value = azurerm_storage_account.identity-storage.primary_connection_string
+}
+
 resource "azurerm_app_service_plan" "app-service-plan" {
     name = "XI-Portal-Web-AppPlan-${var.environment}"
     resource_group_name = azurerm_resource_group.resource-group.name

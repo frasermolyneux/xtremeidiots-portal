825abbd70434f33022a21d5fb004e39add6280aa
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-06 10:10:39 +0100

    Adding very simple list of the users that are in the database

---
+++ src/XI.Portal.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using ElCamino.AspNetCore.Identity.AzureTable.Model;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using XI.Portal.Web.Constants;
+using XI.Portal.Web.Data;
+
+namespace XI.Portal.Web.Controllers
+{
+    [Authorize(Policy = XtremeIdiotsPolicy.SeniorAdmin)]
+    public class UserController : Controller
+    {
+        private readonly ApplicationAuthDbContext _context;
+
+
+        public UserController(ApplicationAuthDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            var query = from user in _context.UserTable.CreateQuery<IdentityUser>()
+                where user.Email != ""
+                select user;
+            var users = query.ToList();
+
+            return View(users);
+        }
+    }
+}
---
+++ src/XI.Portal.Web/Helpers/PolicyTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace XI.Portal.Web.Helpers
+{
+    [HtmlTargetElement(Attributes = "policy")]
+    public class PolicyTagHelper : TagHelper
+    {
+        private readonly IAuthorizationService _authService;
+        private readonly ClaimsPrincipal _principal;
+
+        public PolicyTagHelper(IAuthorizationService authService, IHttpContextAccessor httpContextAccessor)
+        {
+            _authService = authService;
+            _principal = httpContextAccessor.HttpContext.User;
+        }
+
+        public string Policy { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            if (!(await _authService.AuthorizeAsync(_principal, Policy)).Succeeded)
+                output.SuppressOutput();
+        }
+    }
+}

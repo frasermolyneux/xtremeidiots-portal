f34aab49de1c29cb4e96359c8899546dfc4f7f76
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-10 15:42:01 +0100

    Adding game players index feature

--- src/XI.Portal.Web/Controllers/PlayersController.cs
+++ src/XI.Portal.Web/Controllers/PlayersController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using XI.CommonTypes;
 using XI.Portal.Players.Models;
 using XI.Portal.Players.Repository;
 using XI.Portal.Web.Constants;
@@ -20,6 +21,71 @@ namespace XI.Portal.Web.Controllers
         public PlayersController(IPlayersRepository playersRepository)
         {
             _playersRepository = playersRepository ?? throw new ArgumentNullException(nameof(playersRepository));
+        }
+
+        [HttpGet]
+        public IActionResult GamePlayers(GameType id)
+        {
+            ViewData["GameType"] = id;
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> GetGamePlayersPlayersListAjax(GameType id)
+        {
+            var reader = new StreamReader(Request.Body);
+            var requestBody = await reader.ReadToEndAsync();
+
+            var model = JsonConvert.DeserializeObject<DataTableAjaxPostModel>(requestBody);
+
+            if (model == null)
+                return BadRequest();
+
+            var filterModel = new PlayersFilterModel
+            {
+                GameType = id,
+                Filter = PlayersFilterModel.FilterType.UsernameAndGuid
+            };
+            var recordsTotal = await _playersRepository.GetPlayerListCount(filterModel);
+
+            filterModel.FilterString = model.Search?.Value;
+            var recordsFiltered = await _playersRepository.GetPlayerListCount(filterModel);
+
+            filterModel.TakeEntries = model.Length;
+            filterModel.SkipEntries = model.Start;
+
+            if (model.Order == null)
+            {
+                filterModel.Order = PlayersFilterModel.OrderBy.LastSeenDesc;
+            }
+            else
+            {
+                var orderColumn = model.Columns[model.Order.First().Column].Name;
+                var searchOrder = model.Order.First().Dir;
+
+                switch (orderColumn)
+                {
+                    case "username":
+                        filterModel.Order = searchOrder == "asc" ? PlayersFilterModel.OrderBy.UsernameAsc : PlayersFilterModel.OrderBy.UsernameDesc;
+                        break;
+                    case "firstSeen":
+                        filterModel.Order = searchOrder == "asc" ? PlayersFilterModel.OrderBy.FirstSeenAsc : PlayersFilterModel.OrderBy.FirstSeenDesc;
+                        break;
+                    case "lastSeen":
+                        filterModel.Order = searchOrder == "asc" ? PlayersFilterModel.OrderBy.LastSeenAsc : PlayersFilterModel.OrderBy.LastSeenDesc;
+                        break;
+                }
+            }
+
+            var playersListEntries = await _playersRepository.GetPlayerList(filterModel);
+
+            return Json(new
+            {
+                model.Draw,
+                recordsTotal,
+                recordsFiltered,
+                data = playersListEntries
+            });
         }
 
         [HttpGet]

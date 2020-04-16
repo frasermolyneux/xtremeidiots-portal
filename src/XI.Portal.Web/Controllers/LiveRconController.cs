668b10e8b71755f1fdeea9bbdcc7717597c54812
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-16 17:12:35 +0100

    Adding basic kick functionality

--- src/XI.Portal.Web/Controllers/LiveRconController.cs
+++ src/XI.Portal.Web/Controllers/LiveRconController.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using XI.Portal.Auth.Contract.Constants;
 using XI.Portal.Servers.Repository;
+using XI.Servers.Helpers;
+using XI.Servers.Rcon.Factories;
 
 namespace XI.Portal.Web.Controllers
 {
@@ -13,15 +15,19 @@ namespace XI.Portal.Web.Controllers
     public class LiveRconController : Controller
     {
         private readonly IGameServersRepository _gameServersRepository;
+        private readonly IGameServerStatusHelper _gameServerStatusHelper;
         private readonly ILogger<LiveRconController> _logger;
+        private readonly IRconClientFactory _rconClientFactory;
 
         private readonly string[] _requiredClaims = {XtremeIdiotsClaimTypes.SeniorAdmin, XtremeIdiotsClaimTypes.HeadAdmin, XtremeIdiotsClaimTypes.GameAdmin};
 
         public LiveRconController(ILogger<LiveRconController> logger,
-            IGameServersRepository gameServersRepository)
+            IGameServersRepository gameServersRepository,
+            IRconClientFactory rconClientFactory)
         {
             _logger = logger ?? throw new ArgumentNullException(nameof(logger));
             _gameServersRepository = gameServersRepository ?? throw new ArgumentNullException(nameof(gameServersRepository));
+            _rconClientFactory = rconClientFactory ?? throw new ArgumentNullException(nameof(rconClientFactory));
         }
 
         public async Task<IActionResult> Index()
@@ -51,6 +57,23 @@ namespace XI.Portal.Web.Controllers
             {
                 data = model.Players
             });
+        }
+
+        public async Task<IActionResult> KickPlayer(Guid? id, string num)
+        {
+            if (id == null) return NotFound();
+
+            if (string.IsNullOrWhiteSpace(num))
+                return NotFound();
+
+            var model = await _gameServersRepository.GetGameServer(id, User, _requiredClaims);
+
+            var rconClient = _rconClientFactory.CreateInstance(model.GameType, model.Title, model.Hostname, model.QueryPort, model.RconPassword);
+
+            rconClient.KickPlayer(num);
+
+            TempData["Success"] = $"Player in slot {num} has been kicked";
+            return RedirectToAction(nameof(ViewRcon), new {id});
         }
     }
 }

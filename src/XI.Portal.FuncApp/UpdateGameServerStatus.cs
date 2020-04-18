4ce95773f6ab5e983cf63ff02fdc522ab7393133
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-18 15:24:57 +0100

    Fixing crontab

--- src/XI.Portal.FuncApp/UpdateGameServerStatus.cs
+++ src/XI.Portal.FuncApp/UpdateGameServerStatus.cs
@@ -7,7 +7,7 @@ namespace XI.Portal.FuncApp
     public static class UpdateGameServerStatus
     {
         [FunctionName("UpdateGameServerStatus")]
-        public static void Run([TimerTrigger("*/5 * * * *")] TimerInfo myTimer, ILogger log)
+        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         }

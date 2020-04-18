8127864b79c178e38d7d59efb07571d82c900cc7
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-18 16:35:44 +0100

    Adding DI and simple functionality to prove func app

---
+++ src/XI.Portal.FuncApp/Startup.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using XI.AzureTableLogging.Extensions;
+using XI.Portal.Data.Legacy;
+using XI.Portal.FuncApp;
+using XI.Portal.Servers.Extensions;
+
+[assembly: FunctionsStartup(typeof(Startup))]
+
+namespace XI.Portal.FuncApp
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appsettings.json", false, false)
+                .AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("AZURE_FUNCTIONS_ENVIRONMENT")}.json", false, false)
+                .AddJsonFile("local.settings.json", true, false)
+                .AddUserSecrets(Assembly.GetExecutingAssembly(), false)
+                .AddEnvironmentVariables()
+                .Build();
+
+            builder.Services.AddSingleton<IConfiguration>(config);
+
+            builder.Services.AddDbContext<LegacyPortalContext>(options =>
+                options.UseSqlServer(config["LegacyPortalContext:ConnectionString"]));
+
+            builder.Services.AddServersModule(options =>
+            {
+                options.ConfigureGameServersRepository(repositoryOptions => { });
+                options.ConfigureBanFileMonitorsRepository(repositoryOptions => { });
+                options.ConfigureFileMonitorsRepository(repositoryOptions => { });
+                options.ConfigureRconMonitorsRepository(repositoryOptions => { });
+            });
+
+            builder.Services.AddLogging(
+                logging =>
+                {
+                    logging.AddAzureTableLogger(options =>
+                    {
+                        options.CreateTableIfNotExists = true;
+                        options.StorageTableName = config["Logging:AzureTableLogger:StorageTableName"];
+                        options.StorageConnectionString = config["AppDataContainer:StorageConnectionString"];
+                    });
+                });
+        }
+    }
+}
--- src/XI.Portal.FuncApp/UpdateGameServerStatus.cs
+++ src/XI.Portal.FuncApp/UpdateGameServerStatus.cs
@@ -1,15 +1,26 @@
 using System;
+using System.Linq;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
+using XI.Portal.Data.Legacy;
 
 namespace XI.Portal.FuncApp
 {
-    public static class UpdateGameServerStatus
+    public class UpdateGameServerStatus
     {
+        private readonly LegacyPortalContext _legacyContext;
+
+        public UpdateGameServerStatus(LegacyPortalContext legacyContext)
+        {
+            _legacyContext = legacyContext ?? throw new ArgumentNullException(nameof(legacyContext));
+        }
+
         [FunctionName("UpdateGameServerStatus")]
-        public static void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
+        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            foreach (var server in _legacyContext.GameServers.Where(server => server.ShowOnPortalServerList)) log.LogInformation("Updating game server status for {Title}", server.Title);
         }
     }
 }

85407319c2295d0a797774195c7456974a3dca72
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-18 12:23:16 +0100

    Adding function app project

---
+++ src/XI.Portal.FuncApp/UpdateGameServerStatus.cs
@@ -0,0 +1,15 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace XI.Portal.FuncApp
+{
+    public static class UpdateGameServerStatus
+    {
+        [FunctionName("UpdateGameServerStatus")]
+        public static void Run([TimerTrigger("*/5 * * * *")] TimerInfo myTimer, ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+        }
+    }
+}

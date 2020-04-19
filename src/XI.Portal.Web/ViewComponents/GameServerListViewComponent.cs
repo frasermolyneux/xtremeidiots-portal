c9c94ea784ff0539fe51528c9d2b3e9eaf2276f5
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-19 19:08:29 +0100

    Adding home page content and moving game server list to a view component

---
+++ src/XI.Portal.Web/ViewComponents/GameServerListViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XI.Portal.Servers.Interfaces;
+
+namespace XI.Portal.Web.ViewComponents
+{
+    public class GameServerListViewComponent : ViewComponent
+    {
+        private readonly IGameServersRepository _gameServersRepository;
+
+        public GameServerListViewComponent(IGameServersRepository gameServersRepository)
+        {
+            _gameServersRepository = gameServersRepository ?? throw new ArgumentNullException(nameof(gameServersRepository));
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var gameServersBanners = await _gameServersRepository.GetGameServerBanners();
+            return View(gameServersBanners);
+        }
+    }
+}

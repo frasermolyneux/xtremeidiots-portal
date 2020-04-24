73eb0651afae9664360fd23f7329b69798486a95
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-24 12:27:19 +0100

    Reducing replicated admin action code

---
+++ src/XI.Portal.Web/ViewComponents/AdminActionsViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using XI.Portal.Players.Dto;
+
+namespace XI.Portal.Web.ViewComponents
+{
+    public class AdminActionsViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(List<AdminActionDto> adminActions, bool linkToPlayer)
+        {
+            ViewData["LinkToPlayer"] = linkToPlayer;
+            return View(adminActions);
+        }
+    }
+}

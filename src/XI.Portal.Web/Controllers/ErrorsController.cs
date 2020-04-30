1cd3c2d93c21ab1096b36e58da07caeb93159557
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-04-30 10:08:42 +0100

    Fixing errors controller

--- src/XI.Portal.Web/Controllers/ErrorsController.cs
+++ src/XI.Portal.Web/Controllers/ErrorsController.cs
@@ -1,15 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-ions.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-
 namespace XI.Portal.Web.Controllers
 {
     public class ErrorsController : Controller
-  
-           public IActionResult Display(int id)
+    {
+        public IActionResult Display(int id)
         {
             return View(id);
         }

ceb44a1913570ec06a25437c4667f0960f74ff95
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-05-02 19:08:48 +0100

    Fixing duplicate event id

--- src/XI.CommonTypes/EventIds.cs
+++ src/XI.CommonTypes/EventIds.cs
@@ -5,8 +5,8 @@ namespace XI.CommonTypes
     public static class EventIds
     {
         public static EventId AdminAction = new EventId(300, nameof(AdminAction));
+        public static EventId DemoManager = new EventId(400, nameof(DemoManager));
         public static EventId Management { get; set; } = new EventId(100, nameof(Management));
         public static EventId User { get; set; } = new EventId(200, nameof(User));
-        public static EventId DemoManager = new EventId(300, nameof(DemoManager));
     }
 }

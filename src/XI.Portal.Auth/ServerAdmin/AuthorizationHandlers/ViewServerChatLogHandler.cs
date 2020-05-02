8e083ee6add1eacfb4bc5b722ac72a175c660d76
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-05-02 19:32:14 +0100

    Fixing permissions for chat log and links on server admin

--- src/XI.Portal.Auth/ServerAdmin/AuthorizationHandlers/ViewServerChatLogHandler.cs
+++ src/XI.Portal.Auth/ServerAdmin/AuthorizationHandlers/ViewServerChatLogHandler.cs
@@ -2,20 +2,24 @@
 using Microsoft.AspNetCore.Authorization;
 using XI.Portal.Auth.Contract.Constants;
 using XI.Portal.Auth.ServerAdmin.AuthorizationRequirements;
+using XI.Portal.Servers.Dto;
 
 namespace XI.Portal.Auth.ServerAdmin.AuthorizationHandlers
 {
-    public class ViewServerChatLogHandler : AuthorizationHandler<ViewServerChatLog>
+    public class ViewServerChatLogHandler : AuthorizationHandler<ViewServerChatLog, GameServerDto>
     {
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ViewServerChatLog requirement)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ViewServerChatLog requirement, GameServerDto resource)
         {
             if (context.User.HasClaim(claim => claim.Type == XtremeIdiotsClaimTypes.SeniorAdmin))
                 context.Succeed(requirement);
 
-            if (context.User.HasClaim(claim => claim.Type == XtremeIdiotsClaimTypes.HeadAdmin))
+            if (context.User.HasClaim(XtremeIdiotsClaimTypes.HeadAdmin, resource.GameType.ToString()))
                 context.Succeed(requirement);
 
-            if (context.User.HasClaim(claim => claim.Type == XtremeIdiotsClaimTypes.GameAdmin))
+            if (context.User.HasClaim(XtremeIdiotsClaimTypes.GameAdmin, resource.GameType.ToString()))
+                context.Succeed(requirement);
+
+            if (context.User.HasClaim(XtremeIdiotsClaimTypes.Moderator, resource.GameType.ToString()))
                 context.Succeed(requirement);
 
             return Task.CompletedTask;

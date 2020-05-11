49b60927e8e93bbe3616afd9e71972e5db3c9a36
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-05-11 15:24:07 +0100

    Adding method to get the html banners in json form

--- src/XI.Portal.Web/Controllers/BannersController.cs
+++ src/XI.Portal.Web/Controllers/BannersController.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using XI.Portal.Servers.Interfaces;
+using XI.Portal.Servers.Models;
 
 namespace XI.Portal.Web.Controllers
 {
     public class BannersController : Controller
     {
+        private readonly IGameServersRepository _gameServersRepository;
+
+        public BannersController(IGameServersRepository gameServersRepository)
+        {
+            _gameServersRepository = gameServersRepository ?? throw new ArgumentNullException(nameof(gameServersRepository));
+        }
+
         public IActionResult GameServersList()
         {
             return View();
+        }
+
+        public async Task<IActionResult> GetGameServers()
+        {
+            var filterModel = new GameServerFilterModel
+            {
+                Order = GameServerFilterModel.OrderBy.BannerServerListPosition
+            };
+
+            var gameServerDtos = (await _gameServersRepository.GetGameServers(filterModel))
+                .Where(s => s.ShowOnBannerServerList && !string.IsNullOrWhiteSpace(s.HtmlBanner))
+                .ToList();
+
+            var htmlBanners = gameServerDtos.Select(gs => gs.HtmlBanner).ToList();
+
+            return Json(htmlBanners);
         }
     }
 }

1e23357b31a23d7688a22606ce592f0a6f361162
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-06-04 20:57:39 +0100

    Adding application insights

--- terraform/func-app.tf
+++ terraform/func-app.tf
@@ -6,6 +6,13 @@ resource "azurerm_storage_account" "funcapp-storage-account" {
     account_replication_type = "LRS"
 }
 
+resource "azurerm_application_insights" "app-insights" {
+    name = "XI-Portal-AppInsights-${var.environment}"
+    resource_group_name = azurerm_resource_group.resource-group.name
+    location = azurerm_resource_group.resource-group.location
+    application_type = "web"
+}
+
 resource "azurerm_app_service_plan" "funcapp-service-plan" {
     name = "XI-Portal-Func-AppPlan-${var.environment}"
     resource_group_name = azurerm_resource_group.resource-group.name
@@ -24,4 +31,8 @@ resource "azurerm_function_app" "xi-portal-funcapp" {
     storage_connection_string = azurerm_storage_account.funcapp-storage-account.primary_connection_string
     app_service_plan_id = azurerm_app_service_plan.funcapp-service-plan.id
     version = "~3"
+    app_settings {
+        "APPINSIGHTS_INSTRUMENTATIONKEY" = "${azurerm_application_insights.app-insights.instrumentation_key}"
+    }
+    
 }

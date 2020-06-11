0631628440b91ef63631f2b7db6cf4686089b82b
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-06-11 20:50:16 +0100

    hmmm

--- terraform/web-app.tf
+++ terraform/web-app.tf
@@ -37,13 +37,13 @@ resource "cloudflare_record" "frontend-dns" {
   proxied = false
 }
 
-resource "cloudflare_record" "verify-dns" {
-  zone_id = var.hostname_zone_id
-  name = "awverify.${var.hostname}"
-  value = "awverify.${azurerm_app_service.app-service.default_site_hostname}"
-  type = "CNAME"
-  proxied = false
-}
+#resource "cloudflare_record" "verify-dns" {
+#  zone_id = var.hostname_zone_id
+#  name = "awverify.${var.environment}.${var.hostname}"
+#  value = "awverify.${azurerm_app_service.app-service.default_site_hostname}"
+#  type = "CNAME"
+#  proxied = false
+#}
 
 resource "azurerm_app_service_custom_hostname_binding" "custom-hostname" {
   hostname = var.environment

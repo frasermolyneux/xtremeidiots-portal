8d63fbe3a9e4be747407f88fa6affcb08a3d4a9c
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-06-11 22:40:16 +0100

    Back to a known point

--- terraform/func-app.tf
+++ terraform/func-app.tf
@@ -13,25 +13,25 @@ resource "azurerm_application_insights" "app-insights" {
     application_type = "web"
 }
 
-#resource "azurerm_app_service_plan" "funcapp-service-plan" {
-#    name = "XI-Portal-Func-AppPlan-${var.environment}"
-#    resource_group_name = azurerm_resource_group.resource-group.name
-#    location = azurerm_resource_group.resource-group.location
-#    kind = "FunctionApp"
-#    sku {
-#        tier = "Dynamic"
-#        size = "Y1"
-#    }
-#}
+resource "azurerm_app_service_plan" "funcapp-service-plan" {
+    name = "XI-Portal-Func-AppPlan-${var.environment}"
+    resource_group_name = azurerm_resource_group.resource-group.name
+    location = azurerm_resource_group.resource-group.location
+    kind = "FunctionApp"
+    sku {
+        tier = "Dynamic"
+        size = "Y1"
+    }
+}
 
-#resource "azurerm_function_app" "xi-portal-funcapp" {
-#    name = "XI-Portal-FuncApp-${var.environment}"
-#    location = azurerm_resource_group.resource-group.location
-#    resource_group_name = azurerm_resource_group.resource-group.name
-#    storage_connection_string = azurerm_storage_account.funcapp-storage-account.primary_connection_string
-#    app_service_plan_id = azurerm_app_service_plan.app-service-plan.id
-#    version = "~3"
-#    app_settings = {
-#        APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.app-insights.instrumentation_key
-#    }
-#}
+resource "azurerm_function_app" "xi-portal-funcapp" {
+    name = "XI-Portal-FuncApp-${var.environment}"
+    location = azurerm_resource_group.resource-group.location
+    resource_group_name = azurerm_resource_group.resource-group.name
+    storage_connection_string = azurerm_storage_account.funcapp-storage-account.primary_connection_string
+    app_service_plan_id = azurerm_app_service_plan.funcapp-service-plan-plan.id
+    version = "~3"
+    app_settings = {
+        APPINSIGHTS_INSTRUMENTATIONKEY = azurerm_application_insights.app-insights.instrumentation_key
+    }
+}

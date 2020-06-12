64d0737628e05fcabbb9f48216601a61287ad76d
Fraser Molyneux <fraser.molyneux@hotmail.co.uk>
2020-06-12 21:30:41 +0100

    duc command for testing

--- src/XI.Portal.Servers.Integrations/DucCommand.cs
+++ src/XI.Portal.Servers.Integrations/DucCommand.cs
@@ -1,16 +1,34 @@
 ﻿using System;
 using System.Threading.Tasks;
 using XI.Portal.Servers.Integrations.Interfaces;
+using XI.Portal.Servers.Interfaces;
+using XI.Servers.Interfaces;
 
 namespace XI.Portal.Servers.Integrations
 {
     public class DucCommand : IChatCommand
     {
+        private readonly IGameServersRepository _gameServersRepository;
+        private readonly IRconClientFactory _rconClientFactory;
+
+        public DucCommand(
+            IGameServersRepository gameServersRepository, 
+            IRconClientFactory rconClientFactory)
+        {
+            _gameServersRepository = gameServersRepository ?? throw new ArgumentNullException(nameof(gameServersRepository));
+            _rconClientFactory = rconClientFactory ?? throw new ArgumentNullException(nameof(rconClientFactory));
+        }
+
         public string[] CommandAliases { get; } = {"!duc"};
 
-        public Task ProcessMessage(Guid serverId, string name, string guid, string message)
+        public async Task ProcessMessage(Guid serverId, string name, string guid, string message)
         {
-            return Task.CompletedTask;
+            if (name != "Sitting-Duc>XI<")
+                return;
+
+            var server = await _gameServersRepository.GetGameServer(serverId);
+            var rconClient = _rconClientFactory.CreateInstance(server.GameType, server.ServerId, server.Hostname, server.QueryPort, server.RconPassword);
+            await rconClient.Say($"^5Sitting-Duc ^2is the greatest!");
         }
     }
 }

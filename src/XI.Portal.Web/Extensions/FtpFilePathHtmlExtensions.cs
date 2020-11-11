857c00abeb09bad088fbf17b9cc56a2e5c49801c
Fraser Molyneux <websites@mx-mail.io>
2020-11-11 20:45:24 +0000

    Adding embedded link for ban file and log file if the user has FTP permissions

--- src/XI.Portal.Web/Extensions/FtpFilePathHtmlExtensions.cs
+++ src/XI.Portal.Web/Extensions/FtpFilePathHtmlExtensions.cs
@@ -10,5 +10,10 @@ namespace XI.Portal.Web.Extensions
             return new HtmlString(
                 $"ftp://{ftpHostName}{filePath}");
         }
+
+        public static HtmlString MonitorFtpPath(this IHtmlHelper html, string ftpHostName, string filePath, string ftpUsername, string ftpPassword)
+        {
+            return new HtmlString($"<a target=\"_blank\" href=\"ftp://{ftpUsername}:{ftpPassword}@{ftpHostName}{filePath}\">ftp://{ftpHostName}{filePath}</a>");
+        }
     }
 }

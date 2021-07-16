9dbaea80cb0165cf1eafab832d166ef00e0c8614
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-07-16 14:37:19 +0100

    Such an idiot.

--- terraform/service-bus.tf
+++ terraform/service-bus.tf
@@ -13,34 +13,26 @@ resource "azurerm_servicebus_queue" "servicebus-queue" {
   enable_partitioning = true
 }
 
-resource "azurerm_eventhub_namespace_authorization_rule" "servicebus-function-authrule" {
-  name                = "function-authrule"
-  namespace_name      = azurerm_servicebus_namespace.servicebus.name
+resource "azurerm_servicebus_namespace_authorization_rule" "servicebus-function-authrule" {
+  name = "function-authrule"
+  namespace_name = azurerm_servicebus_namespace.servicebus.name
   resource_group_name = azurerm_resource_group.resource-group.name
 
   listen = true
   send   = true
   manage = true
-
-  depends_on = [
-    azurerm_servicebus_namespace.servicebus
-  ]
 }
 
 output "servicebus_connection_string" {
-  value = azurerm_eventhub_namespace_authorization_rule.servicebus-function-authrule.primary_connection_string
+  value = azurerm_servicebus_namespace_authorization_rule.servicebus-function-authrule.primary_connection_string
 }
 
-resource "azurerm_eventhub_namespace_authorization_rule" "servicebus-bot-authrule" {
-  name                = "bot-authrule"
-  namespace_name      = azurerm_servicebus_namespace.servicebus.name
+resource "azurerm_servicebus_namespace_authorization_rule" "servicebus-bot-authrule" {
+  name = "bot-authrule"
+  namespace_name = azurerm_servicebus_namespace.servicebus.name
   resource_group_name = azurerm_resource_group.resource-group.name
 
   listen = false
   send   = true
   manage = false
-
-  depends_on = [
-    azurerm_servicebus_namespace.servicebus
-  ]
 }

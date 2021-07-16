379107846d41d94e64485abc495c99a714221ee9
Fraser Molyneux <frasermolyneux@mx-mail.io>
2021-07-16 10:12:24 +0100

    Renaming resource

--- terraform/service-bus.tf
+++ terraform/service-bus.tf
@@ -1,5 +1,5 @@
 resource "azurerm_servicebus_namespace" "servicebus" {
-  name = "tfex-servicebus-namespace"
+  name = "xi-portal-sbns-${var.environment}"
   location = azurerm_resource_group.resource-group.location
   resource_group_name = azurerm_resource_group.resource-group.name
   sku = "Basic"

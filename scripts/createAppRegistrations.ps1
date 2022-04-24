17ca8b09cb9ef7ba885c78a653ff9575f69f2613
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-04-24 15:24:30 +0100

    Tweaks

--- scripts/createAppRegistrations.ps1
+++ scripts/createAppRegistrations.ps1
@@ -16,17 +16,15 @@ if ($null -eq $eventsApiSp) {
     az ad sp create --id $eventsApiId
 }
 
-az ad app update --id $eventsApiId --app-roles @app-registrations\events-api-approles.json
+az ad app update --id $eventsApiId --app-roles @app-registrations/events-api-approles.json
 
 $b3BotsClientPermissions = (az ad app permission list --id $b3botsClientId) | ConvertFrom-Json
 $b3BotsClientPermissions
 
-if (($b3BotsClientPermissions.resourceAccess | Where-Object {$_.id -eq '85755f6f-35d9-476a-a680-b7e1a12a0e16'}).Count -eq 0) {
+if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '85755f6f-35d9-476a-a680-b7e1a12a0e16' }).Count -eq 0) {
     az ad app permission add --id $b3botsClientId --api $eventsApiId --api-permissions '85755f6f-35d9-476a-a680-b7e1a12a0e16=Role' 
 }
 
-if (($b3BotsClientPermissions.resourceAccess | Where-Object {$_.id -eq '042608f6-317d-43b7-8fad-4618705abfcc'}).Count -eq 0) {
+if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '042608f6-317d-43b7-8fad-4618705abfcc' }).Count -eq 0) {
     az ad app permission add --id $b3botsClientId --api $eventsApiId --api-permissions '042608f6-317d-43b7-8fad-4618705abfcc=Role' 
 }
-
-az ad app permission admin-consent --id $b3botsClientId 

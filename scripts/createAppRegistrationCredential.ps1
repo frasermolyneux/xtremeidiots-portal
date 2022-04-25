87b43945c92813510c3aecbb0f1f9f789978d0c9
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-04-25 17:13:27 +0100

    Renaming eventsApi to eventsApp

--- scripts/createAppRegistrationCredential.ps1
+++ scripts/createAppRegistrationCredential.ps1
@@ -5,23 +5,23 @@ param (
     $secretIdentifier
 )
 
-$eventsApiId = (az ad app list --filter "displayName eq '$applicationName'" --query '[].appId') | ConvertFrom-Json
-$credentials = (az ad app credential list --id $eventsApiId) | ConvertFrom-Json
+$applicationId = (az ad app list --filter "displayName eq '$applicationName'" --query '[].appId') | ConvertFrom-Json
+$credentials = (az ad app credential list --id $applicationId) | ConvertFrom-Json
 
 if ($credentials.Count -eq 0) {
-    $credential = (az ad app credential reset --id $eventsApiId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }
 
 if ($credentials.Count -eq 1) {
-    $credential = (az ad app credential reset --id $eventsApiId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }
 
 if ($credentials.Count -eq 2) {
     $credentialToDelete = $credentials | Sort-Object { Get-Date($_.endDate) } | Select-Object -First 1
-    az ad app credential delete --id $eventsApiId --key-id $credentialToDelete.keyId
+    az ad app credential delete --id $applicationId --key-id $credentialToDelete.keyId
 
-    $credential = (az ad app credential reset --id $eventsApiId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }
--- scripts/createAppRegistrations.ps1
+++ scripts/createAppRegistrations.ps1
@@ -7,26 +7,26 @@ if ($null -eq $b3BotsClientSp) {
     az ad sp create --id $b3BotsClientId
 }
 
-$eventsApiName = 'portal-events-api-prd'
-az ad app create --display-name $eventsApiName --identifier-uris "api://$eventsApiName"
-$eventsApiId = (az ad app list --filter "displayName eq '$eventsApiName'" --query '[].appId') | ConvertFrom-Json
+$eventsApiAppName = 'portal-events-api-prd'
+az ad app create --display-name $eventsApiAppName --identifier-uris "api://$eventsApiAppName"
+$eventsApiAppId = (az ad app list --filter "displayName eq '$eventsApiAppName'" --query '[].appId') | ConvertFrom-Json
 
-$eventsApiSp = az ad sp show --id $eventsApiId
-if ($null -eq $eventsApiSp) {
-    az ad sp create --id $eventsApiId
+$eventsApiAppSp = az ad sp show --id $eventsApiAppId
+if ($null -eq $eventsApiAppSp) {
+    az ad sp create --id $eventsApiAppId
 }
 
-az ad app update --id $eventsApiId --app-roles @app-registrations/events-api-approles.json
+az ad app update --id $eventsApiAppId --app-roles @app-registrations/events-api-approles.json
 
 $b3BotsClientPermissions = (az ad app permission list --id $b3botsClientId) | ConvertFrom-Json
 $b3BotsClientPermissions
 
 if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '85755f6f-35d9-476a-a680-b7e1a12a0e16' }).Count -eq 0) {
-    az ad app permission add --id $b3botsClientId --api $eventsApiId --api-permissions '85755f6f-35d9-476a-a680-b7e1a12a0e16=Role' 
+    az ad app permission add --id $b3botsClientId --api $eventsApiAppId --api-permissions '85755f6f-35d9-476a-a680-b7e1a12a0e16=Role' 
 }
 
 if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '042608f6-317d-43b7-8fad-4618705abfcc' }).Count -eq 0) {
-    az ad app permission add --id $b3botsClientId --api $eventsApiId --api-permissions '042608f6-317d-43b7-8fad-4618705abfcc=Role' 
+    az ad app permission add --id $b3botsClientId --api $eventsApiAppId --api-permissions '042608f6-317d-43b7-8fad-4618705abfcc=Role' 
 }
 
 $repositoryApiName = 'portal-repository-api-prd'

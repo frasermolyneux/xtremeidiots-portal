d53c241548a1322e3a9dd65f1f2b31a55af3034a
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-04-30 21:19:44 +0100

    Fixing permissions issue on view game chat log

--- src/XI.Portal.Auth/ServerAdmin/AuthorizationHandlers/ViewGameChatLogHandler.cs
+++ src/XI.Portal.Auth/ServerAdmin/AuthorizationHandlers/ViewGameChatLogHandler.cs
@@ -1,22 +1,21 @@
-﻿using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
-using XI.CommonTypes;
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Threading.Tasks;
 using XI.Portal.Auth.Contract.Constants;
 using XI.Portal.Auth.ServerAdmin.AuthorizationRequirements;
 
 namespace XI.Portal.Auth.ServerAdmin.AuthorizationHandlers
 {
-    public class ViewGameChatLogHandler : AuthorizationHandler<ViewGameChatLog, GameType>
+    public class ViewGameChatLogHandler : AuthorizationHandler<ViewGameChatLog, string>
     {
-        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ViewGameChatLog requirement, GameType resource)
+        protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, ViewGameChatLog requirement, string resource)
         {
             if (context.User.HasClaim(claim => claim.Type == XtremeIdiotsClaimTypes.SeniorAdmin))
                 context.Succeed(requirement);
 
-            if (context.User.HasClaim(XtremeIdiotsClaimTypes.HeadAdmin, resource.ToString()))
+            if (context.User.HasClaim(XtremeIdiotsClaimTypes.HeadAdmin, resource))
                 context.Succeed(requirement);
 
-            if (context.User.HasClaim(XtremeIdiotsClaimTypes.GameAdmin, resource.ToString()))
+            if (context.User.HasClaim(XtremeIdiotsClaimTypes.GameAdmin, resource))
                 context.Succeed(requirement);
 
             return Task.CompletedTask;

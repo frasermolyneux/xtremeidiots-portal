ddfc8f585537172319824cdf0ac807c67bc61d78
Fraser Molyneux <websites@mx-mail.io>
2022-05-03 19:48:39 +0100

    Adding role assignment for SQL server

---
+++ scripts/CreateSqlServerRoleAssignment.ps1
@@ -0,0 +1,13 @@
+param {
+    $sqlServerName,
+    $resourceGroup
+}
+
+$sqlServer = az sql server show --name $sqlServerName --resource-group $resourceGroup | ConvertFrom-Json
+$principalId = $sqlServer.identity.principalId
+
+$assignments = (az rest -m GET -u "https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments?`$filter=principalId eq '$principalId'") | ConvertFrom-Json
+if ($assignments.value.count -eq 0)
+{
+    az rest -m POST -u https://graph.microsoft.com/v1.0/roleManagement/directory/roleAssignments -b "{'@odata.type': '#microsoft.graph.unifiedRoleAssignment', 'principalId': '$principalId', 'roleDefinitionId': '88d8e3e3-8f55-4a1e-953a-9b9898b8876b','directoryScopeId': '/'}"
+}

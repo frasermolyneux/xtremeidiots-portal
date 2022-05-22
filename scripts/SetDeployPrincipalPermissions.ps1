c6d6ce7a898f5eec729adfc5020eaaae704103e1
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-05-22 09:21:43 +0100

    Hardcode principal ID for now

--- scripts/SetDeployPrincipalPermissions.ps1
+++ scripts/SetDeployPrincipalPermissions.ps1
@@ -2,7 +2,7 @@ param (
     $environment
 )
 
-$servicePrincipalId = $env:servicePrincipalId
+$servicePrincipalId = '1f0d4d49-754e-4d18-88f4-c0a3a6d5d6fe'
 
 $spnMember = (az ad group member check --group "sg-sql-portal-$environment-admins" --member-id $servicePrincipalId) | ConvertFrom-Json
 if ($spnMember.value -eq $false) {

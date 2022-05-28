7f4bf757c964950a7b5a1c6c4f276e0aca804932
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-05-28 09:38:00 +0100

    Removing legacy suncapp scripts

--- scripts/SetLegacyFuncAppPermissions.ps1
+++
@@ -1,8 +0,0 @@
-param (
-    $environment
-)
-
-$identity = az functionapp identity show --name portal-funcapp-leo --resource-group xi-portal-leo | ConvertFrom-Json
-$principalId = $identity.principalId
-
-. "./scripts/functions/GrantRepositoryApiPermissionsToApp.ps1" -principalId $principalId -environment $environment

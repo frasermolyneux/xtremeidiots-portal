23fcdbf2f995d5c9150bb67c053e0c0ca7ec6d0b
Fraser Molyneux <websites@mx-mail.io>
2022-06-09 19:23:31 +0100

    Updating from breaking change

--- scripts/createAppRegistrationCredential.ps1
+++ scripts/createAppRegistrationCredential.ps1
@@ -11,12 +11,12 @@ $credentials = (az ad app credential list --id $applicationId) | ConvertFrom-Jso
 $credentials | Write-Host
 
 if ($credentials.Count -eq 0) {
-    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --display-name $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }
 
 if ($credentials.Count -eq 1) {
-    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --display-name $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }
 
@@ -24,6 +24,6 @@ if ($credentials.Count -eq 2) {
     $credentialToDelete = $credentials | Sort-Object { Get-Date($_.endDateTime) } | Select-Object -First 1
     az ad app credential delete --id $applicationId --key-id $credentialToDelete.keyId
 
-    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --credential-description $secretIdentifier) | ConvertFrom-Json
+    $credential = (az ad app credential reset --id $applicationId  --append --years 2 --display-name $secretIdentifier) | ConvertFrom-Json
     az keyvault secret set --name $secretName --vault-name $keyVaultName --value $credential.password --description 'text/plain'
 }

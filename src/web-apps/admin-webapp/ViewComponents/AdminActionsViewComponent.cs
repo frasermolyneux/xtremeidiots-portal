943fa034a51971a622521fb4d61c063adbfa9c58
Fraser Molyneux <websites@mx-mail.io>
2022-06-13 21:14:56 +0100

    Couple of hacky bugfixes

--- src/web-apps/admin-webapp/ViewComponents/AdminActionsViewComponent.cs
+++ src/web-apps/admin-webapp/ViewComponents/AdminActionsViewComponent.cs
@@ -1,13 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
+
 using XtremeIdiots.Portal.RepositoryApi.Abstractions.Models.AdminActions;
+using XtremeIdiots.Portal.RepositoryApi.Abstractions.Models.Players;
 
 namespace XtremeIdiots.Portal.AdminWebApp.ViewComponents
 {
     public class AdminActionsViewComponent : ViewComponent
     {
-        public IViewComponentResult Invoke(List<AdminActionDto> adminActions, bool linkToPlayer)
+        public IViewComponentResult Invoke(List<AdminActionDto> adminActions, bool linkToPlayer, PlayerDto? playerDto)
         {
+            if (playerDto != null)
+                ViewData["Player"] = playerDto;
+
             ViewData["LinkToPlayer"] = linkToPlayer;
             return View(adminActions);
         }

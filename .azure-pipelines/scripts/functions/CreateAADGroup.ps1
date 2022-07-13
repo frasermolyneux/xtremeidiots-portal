e07f7aa1c652b37595cdc701629e12327f810065
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-13 20:00:00 +0100

    params

--- .azure-pipelines/scripts/functions/CreateAADGroup.ps1
+++ .azure-pipelines/scripts/functions/CreateAADGroup.ps1
@@ -5,7 +5,7 @@ param (
 $group = (az ad group list --filter "displayName eq '$groupName'") | ConvertFrom-Json
 if ($group.Count -eq 0) {
     Write-Host "AAD Group '$groupName' does not exist, creating."
-    az ad group create --display-name $groupName --mail-nickname $groupName
+    az ad group create --display-name "$groupName" --mail-nickname "$groupName"
 }
 else {
     Write-Host "AAD Group '$groupName' already exists, doing nothing."
--- .azure-pipelines/scripts/functions/CreateAppRegistration.ps1
+++ .azure-pipelines/scripts/functions/CreateAppRegistration.ps1
@@ -3,16 +3,16 @@ param (
     $appRoles = $null
 )
 
-az ad app create --display-name $applicationName --identifier-uris "api://$applicationName" | Out-Null
+az ad app create --display-name "$applicationName" --identifier-uris "api://$applicationName" | Out-Null
 $applicationId = (az ad app list --filter "displayName eq '$applicationName'" --query '[].appId') | ConvertFrom-Json
 
-az ad app update --id $applicationId --sign-in-audience 'AzureADMyOrg' --enable-id-token-issuance true --enable-access-token-issuance false | Out-Null
+az ad app update --id "$applicationId" --sign-in-audience 'AzureADMyOrg' --enable-id-token-issuance true --enable-access-token-issuance false | Out-Null
 
-$applicationServicePrincipal = az ad sp show --id $applicationId
+$applicationServicePrincipal = az ad sp show --id "$applicationId"
 if ($null -eq $applicationServicePrincipal) {
-    az ad sp create --id $applicationId | Out-Null
+    az ad sp create --id "$applicationId" | Out-Null
 }
 
 if ($null -ne $appRoles) {
-    az ad app update --id $applicationId --app-roles .azure-pipelines/app-registrations/$appRoles | Out-Null
+    az ad app update --id "$applicationId" --app-roles ".azure-pipelines/app-registrations/$appRoles" | Out-Null
 }

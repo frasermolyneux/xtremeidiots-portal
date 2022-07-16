205a63ee1af404c7c17df75b2d22e12e96d59259
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-16 14:03:00 +0100

    Add missing deploy principal permissions

--- .azure-pipelines/scripts/SetDeployPrincipalPermissions.ps1
+++ .azure-pipelines/scripts/SetDeployPrincipalPermissions.ps1
@@ -4,3 +4,5 @@ param (
 )
 
 az keyvault set-policy --name "kv-portal-$environment-$location" --spn $env:servicePrincipalId --secret-permissions get set | Out-Null
+
+. "./.azure-pipelines/scripts/functions/AddPrincipalToAADGroup.ps1" -principalId $env:servicePrincipalId -groupName "sg-sql-platform-$environment-admins"

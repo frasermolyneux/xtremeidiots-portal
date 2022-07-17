bb7600e08a18839ded3b06500d2a3d4c69e7e087
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-17 19:17:36 +0100

    Adding some additional debugging

--- .azure-pipelines/scripts/functions/AddPrincipalToAADGroup.ps1
+++ .azure-pipelines/scripts/functions/AddPrincipalToAADGroup.ps1
@@ -3,7 +3,13 @@ param (
     $groupName
 )
 
+Write-Host "Ensuring principal with id '$principalId' is a member of '$groupName'"
+
 $member = (az ad group member check --group $groupName --member-id $principalId) | ConvertFrom-Json
 if ($member.value -eq $false) {
+    Write-Host "Adding principal with id '$principalId' as a member of '$groupName'"
     az ad group member add --group $groupName --member-id $principalId
+}
+else {
+    Write-Host "Principal with id '$principalId' is already a member of '$groupName' - doing nothing"
 }
--- .azure-pipelines/scripts/functions/CreateAADGroup.ps1
+++ .azure-pipelines/scripts/functions/CreateAADGroup.ps1
@@ -2,6 +2,8 @@ param (
     $groupName
 )
 
+Write-Host "Ensuring AAD group with name '$groupName' exists"
+
 $group = (az ad group list --filter "displayName eq '$groupName'") | ConvertFrom-Json
 if ($group.Count -eq 0) {
     Write-Host "AAD Group '$groupName' does not exist, creating."

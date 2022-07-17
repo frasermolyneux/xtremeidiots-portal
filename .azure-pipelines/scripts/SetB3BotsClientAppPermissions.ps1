1753d15993ec4b0c5b8097f6ed46d681b35204e0
Fraser Molyneux <frasermolyneux@mx-mail.io>
2022-07-17 19:59:50 +0100

    Idk what this script was/is...

--- .azure-pipelines/scripts/SetB3BotsClientAppPermissions.ps1
+++ .azure-pipelines/scripts/SetB3BotsClientAppPermissions.ps1
@@ -6,13 +6,17 @@ param (
 $b3BotsClientName = "portal-b3bots-client-$environment"
 $b3BotsClientId = (az ad app list --filter "displayName eq '$b3BotsClientName'" --query '[].appId') | ConvertFrom-Json
 
+$appName = "portal-events-api-$environment"
+$appId = (az ad app list --filter "displayName eq '$appName'" --query '[].appId') | ConvertFrom-Json
+$appSpnId = (az ad sp list --filter "appId eq '$appId'" --query '[0].id') | ConvertFrom-Json
+
 $b3BotsClientPermissions = (az ad app permission list --id "$b3botsClientId") | ConvertFrom-Json
 $b3BotsClientPermissions
 
 if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '85755f6f-35d9-476a-a680-b7e1a12a0e16' }).Count -eq 0) {
-    az ad app permission add --id "$b3botsClientId" --api "$eventsApiAppId" --api-permissions '85755f6f-35d9-476a-a680-b7e1a12a0e16=Role' 
+    az ad app permission add --id "$b3botsClientId" --api "$appSpnId" --api-permissions '85755f6f-35d9-476a-a680-b7e1a12a0e16=Role' 
 }
 
 if (($b3BotsClientPermissions.resourceAccess | Where-Object { $_.id -eq '042608f6-317d-43b7-8fad-4618705abfcc' }).Count -eq 0) {
-    az ad app permission add --id "$b3botsClientId" --api "$eventsApiAppId" --api-permissions '042608f6-317d-43b7-8fad-4618705abfcc=Role' 
+    az ad app permission add --id "$b3botsClientId" --api "$appSpnId" --api-permissions '042608f6-317d-43b7-8fad-4618705abfcc=Role' 
 }

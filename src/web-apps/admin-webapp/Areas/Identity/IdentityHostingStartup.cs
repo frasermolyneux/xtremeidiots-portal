e1a74f6aa739cdbc3fe9d10bb77477b147bcd6b8
Fraser Molyneux <frasermolyneux@mx-mail.io>
2023-02-06 19:47:07 +0000

    Fixing bug where user is not redirected when not logged in

--- src/web-apps/admin-webapp/Areas/Identity/IdentityHostingStartup.cs
+++ src/web-apps/admin-webapp/Areas/Identity/IdentityHostingStartup.cs
@@ -42,52 +42,56 @@ namespace XtremeIdiots.Portal.AdminWebApp.Areas.Identity
 
                 services.Configure<SecurityStampValidatorOptions>(options => { options.ValidationInterval = TimeSpan.FromMinutes(15); });
 
-                services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-                    .AddCookie(options =>
+                services.AddAuthentication(options =>
+                {
+                    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                })
+                .AddCookie(options =>
+                {
+                    options.AccessDeniedPath = "/Errors/Display/401";
+                    options.Cookie.Name = "XIPortal";
+                    options.Cookie.HttpOnly = true;
+                    options.ExpireTimeSpan = TimeSpan.FromDays(7);
+                    options.LoginPath = "/Identity/Login";
+                    options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
+                    options.SlidingExpiration = true;
+                })
+                .AddOAuth("XtremeIdiots", options =>
+                {
+                    options.ClientId = context.Configuration["xtremeidiots_auth_client_id"];
+                    options.ClientSecret = context.Configuration["xtremeidiots_auth_client_secret"];
+
+                    options.CallbackPath = new PathString("/signin-xtremeidiots");
+
+                    options.AuthorizationEndpoint = "https://www.xtremeidiots.com/oauth/authorize/";
+                    options.TokenEndpoint = "https://www.xtremeidiots.com/oauth/token/";
+                    options.UserInformationEndpoint = "https://www.xtremeidiots.com/api/core/me";
+
+                    options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
+                    options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
+                    options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
+
+                    options.Scope.Add("profile");
+
+                    options.Events = new OAuthEvents
                     {
-                        options.AccessDeniedPath = "/Errors/Display/401";
-                        options.Cookie.Name = "XIPortal";
-                        options.Cookie.HttpOnly = true;
-                        options.ExpireTimeSpan = TimeSpan.FromDays(7);
-                        options.LoginPath = "/Identity/Login";
-                        options.ReturnUrlParameter = CookieAuthenticationDefaults.ReturnUrlParameter;
-                        options.SlidingExpiration = true;
-                    })
-                    .AddOAuth("XtremeIdiots", options =>
-                    {
-                        options.ClientId = context.Configuration["xtremeidiots_auth_client_id"];
-                        options.ClientSecret = context.Configuration["xtremeidiots_auth_client_secret"];
-
-                        options.CallbackPath = new PathString("/signin-xtremeidiots");
-
-                        options.AuthorizationEndpoint = "https://www.xtremeidiots.com/oauth/authorize/";
-                        options.TokenEndpoint = "https://www.xtremeidiots.com/oauth/token/";
-                        options.UserInformationEndpoint = "https://www.xtremeidiots.com/api/core/me";
-
-                        options.ClaimActions.MapJsonKey(ClaimTypes.NameIdentifier, "id");
-                        options.ClaimActions.MapJsonKey(ClaimTypes.Name, "name");
-                        options.ClaimActions.MapJsonKey(ClaimTypes.Email, "email");
-
-                        options.Scope.Add("profile");
-
-                        options.Events = new OAuthEvents
+                        OnCreatingTicket = async context =>
                         {
-                            OnCreatingTicket = async context =>
-                            {
-                                var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
-                                request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-                                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
+                            var request = new HttpRequestMessage(HttpMethod.Get, context.Options.UserInformationEndpoint);
+                            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", context.AccessToken);
 
-                                var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
-                                response.EnsureSuccessStatusCode();
+                            var response = await context.Backchannel.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, context.HttpContext.RequestAborted);
+                            response.EnsureSuccessStatusCode();
 
-                                var contentAsString = await response.Content.ReadAsStringAsync();
-                                var user = JsonDocument.Parse(contentAsString);
+                            var contentAsString = await response.Content.ReadAsStringAsync();
+                            var user = JsonDocument.Parse(contentAsString);
 
-                                context.RunClaimActions(user.RootElement);
-                            }
-                        };
-                    });
+                            context.RunClaimActions(user.RootElement);
+                        }
+                    };
+                });
 
                 services.AddDataProtection()
                     .SetApplicationName("portal")

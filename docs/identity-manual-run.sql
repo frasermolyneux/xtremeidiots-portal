624ba2e461e97cb1d47193534953187dc6ba6f00
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-07-06 18:05:57 +0100

    Update manual script

--- docs/identity-manual-run.sql
+++ docs/identity-manual-run.sql
@@ -1,15 +1,15 @@
-IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-platform-prd-portalidentitydb-prd-readers'))  
+IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sql-portal-core-prd-portal-web-l6supxzf6itfq-readers'))  
 BEGIN  
-	PRINT 'Adding user: sg-sql-platform-prd-portalidentitydb-prd-readers to [db_datareader]'
-	CREATE USER [sg-sql-platform-prd-portalidentitydb-prd-readers] FROM EXTERNAL PROVIDER
-	ALTER ROLE [db_datareader] ADD MEMBER [sg-sql-platform-prd-portalidentitydb-prd-readers]
-	SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-platform-prd-portalidentitydb-prd-readers'
+	PRINT 'Adding user: sql-portal-core-prd-portal-web-l6supxzf6itfq-readers to [db_datareader]'
+	CREATE USER [sql-portal-core-prd-portal-web-l6supxzf6itfq-readers] FROM EXTERNAL PROVIDER
+	ALTER ROLE [db_datareader] ADD MEMBER [sql-portal-core-prd-portal-web-l6supxzf6itfq-readers]
+	SELECT * FROM sys.database_principals WHERE [name] = 'sql-portal-core-prd-portal-web-l6supxzf6itfq-readers'
 END  
 
-IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-platform-prd-portalidentitydb-prd-writers'))  
+IF (NOT EXISTS(SELECT * FROM sys.database_principals WHERE [name] = 'sql-portal-core-prd-portal-web-l6supxzf6itfq-writers'))  
 BEGIN  
-	PRINT 'Adding user: sg-sql-platform-prd-portalidentitydb-prd-writers to [db_datawriter]'
-	CREATE USER [sg-sql-platform-prd-portalidentitydb-prd-writers] FROM EXTERNAL PROVIDER
-	ALTER ROLE [db_datawriter] ADD MEMBER [sg-sql-platform-prd-portalidentitydb-prd-writers]
-	SELECT * FROM sys.database_principals WHERE [name] = 'sg-sql-platform-prd-portalidentitydb-prd-writers'
+	PRINT 'Adding user: sql-portal-core-prd-portal-web-l6supxzf6itfq-writers to [db_datawriter]'
+	CREATE USER [sql-portal-core-prd-portal-web-l6supxzf6itfq-writers] FROM EXTERNAL PROVIDER
+	ALTER ROLE [db_datawriter] ADD MEMBER [sql-portal-core-prd-portal-web-l6supxzf6itfq-writers]
+	SELECT * FROM sys.database_principals WHERE [name] = 'sql-portal-core-prd-portal-web-l6supxzf6itfq-writers'
 END  
